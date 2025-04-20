9d51c5cd58e136919ba8c3b7b7dcd38dc8c52609
EGladki <e.gladki@gmail.com>
2025-04-20 18:18:16 +0300

    Add exchange round to 2 decimal places

--- src/main/java/com/gladkiei/exchanger/dto/ExchangeResponseDTO.java
+++ src/main/java/com/gladkiei/exchanger/dto/ExchangeResponseDTO.java
@@ -4,6 +4,9 @@ import com.gladkiei.exchanger.models.Currency;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Setter
 @Getter
 public class ExchangeResponseDTO {
@@ -11,13 +14,13 @@ public class ExchangeResponseDTO {
     private Currency targetCurrency;
     private double rate;
     private double amount;
-    private double convertedAmount;
+    private BigDecimal convertedAmount;
 
     public ExchangeResponseDTO(Currency baseCurrency, Currency targetCurrency, double rate, double amount) {
         this.baseCurrency = baseCurrency;
         this.targetCurrency = targetCurrency;
         this.rate = rate;
         this.amount = amount;
-        convertedAmount = rate * amount;
+        convertedAmount = BigDecimal.valueOf(rate * amount).setScale(2, RoundingMode.DOWN);
     }
 }
